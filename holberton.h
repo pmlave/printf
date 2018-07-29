a5ce77c7d1d772b39705f218ccb639ac8e1e5450
pmlave <pmlave@gmail.com>
2018-07-29 20:33:43 +0000

    Updated header file

--- holberton.h
+++ holberton.h
@@ -1,5 +1,10 @@
 #ifndef _HOLBRTON_H_
 #define _HOLBERTON_H_
+typdef struct specs
+{
+	char *specifier;
+	void (*func)(char *);
+} specs_t;
 
 int _printf(const char *format, ...);
 int _putchar(char c);
