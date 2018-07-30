430218bdbf9d9054c4f74f1bbc133cde96ad0960
pmlave <pmlave@gmail.com>
2018-07-30 01:07:57 +0000

    Updating betty styling

--- get_func.c
+++ get_func.c
@@ -1,7 +1,11 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdarg.h>
-
+/**
+ * get_func - pointer to evaluating function
+ * @s: Character passed in to compare
+ * Return: Number of characters printed
+ */
 int (*get_func(char s))(va_list)
 {
 	specs_t specs[] = {
--- print_type.c
+++ print_type.c
@@ -2,13 +2,24 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdarg.h>
-
+/**
+ * print_char - printing a single character
+ * @list: Va_list of values
+ * Return: Int of number of characters
+ */
 int print_char(va_list list)
 {
 	char c = va_arg(list, int);
+
 	write(1, &c, 1);
 	return (0);
 }
+/**
+ * print_string - print a string of characters
+ * @list: Va_list of values
+ * Return: Number of characters printed
+ */
+
 int print_string(va_list list)
 {
 	char *c = va_arg(list, char *);
