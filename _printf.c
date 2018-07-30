f923dac79a721bc108e48e5ca85f0fa0a3849cb5
pmlave <pmlave@gmail.com>
2018-07-30 02:47:01 +0000

    Updating printf function

--- _printf.c
+++ _printf.c
@@ -9,21 +9,25 @@
  */
 int _printf(const char *format, ...)
 {
-	int i;
+	int i, a = 0;
 	va_list list;
 
 	va_start(list, format);
 		for (i = 0; format[i] != '\0'; i++)
 		{
 			if (format[i] == '%')
-				get_func(format[i + 1])(list);
-			else if (format[i] == '\\')
 			{
-				if (format[i + 1] == 'n')
-					_putchar('\n');
+				if (format[i + 1] == '%')
+					_putchar('%');
+				else
+					get_func(format[i + 1])(list);
+				i++;
 			}
 			else
+			{
 				_putchar(format[i]);
+				a++;
+			}
 		}
-		return (0);
+		return (a);
 }
